4f9f865d8602086ee222abe51e7181d096972a63
pauline muthoni <kinyanjuipauline69@gmail.com>
2025-04-10 15:26:21 +0300

    add adout.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from "./links";
 
-function About() {
+function About(user) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {user.bio && <p>{user.bio}</p>}
+  
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github = {user.links.github} linkedin = {user.links.linkedin} />
     </div>
   );
 }
